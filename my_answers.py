aef80740945ded04b5a32f6ed85dfc6c832859a4
Phien Pham <phiendpham@gmail.com>
2018-05-13 10:14:10 +0700

    Implement transforming the input series
    - Set window-size into a set of input/output pairs for use with RNN model.

--- my_answers.py
+++ my_answers.py
@@ -6,14 +6,16 @@ from keras.layers import LSTM
 import keras
 
 
-# TODO: fill out the function below that transforms the input series 
-# and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
 
-    # reshape each 
+    for i in range(0, len(series) - window_size):
+        X.append(series[i: i + window_size])
+        y.append(series[i:window_size])
+
+    # reshape each
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
@@ -26,7 +28,7 @@ def build_part1_RNN(window_size):
     pass
 
 
-### TODO: return the text input with only ascii lowercase and the punctuation given below included.
+### TODO: return the tex input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
 
@@ -40,7 +42,7 @@ def window_transform_text(text, window_size, step_size):
 
     return inputs,outputs
 
-# TODO build the required RNN model: 
-# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
+# TODO build the required RNN model:
+# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss
 def build_part2_RNN(window_size, num_chars):
     pass
